52f73f4d8ae1729d92370fa0e43300f568ba99d4
Grigory Frantsuzov <gfrantsuzov@systemorph.com>
2019-10-06 12:47:19 +0300

    to console

--- grabbers/flickr.py
+++ grabbers/flickr.py
@@ -1,12 +1,13 @@
 import flickr_api
+import urllib
 import os
+import sys
 import random 
 
-key = ''
-secret = ''
+DOWNLOADS_DIR = 'downloads'
 
-def get_url(text):
-    
+def get_url(text,key,secret):
+    print ('start')
     flickr_api.set_keys(api_key=key, api_secret=secret)
 
     w = flickr_api.Walker(
@@ -25,5 +26,22 @@ def get_url(text):
         links_to_photo.append(photo.getPhotoFile())
     return links_to_photo
 
-if __name__ == '__main__':
-    print(get_url("hack"))
+def main(query, key, secret):
+    for link in get_url(query,key,secret):
+        link = link.strip()
+        name = link.rsplit('/', 1)[-1]
+        filename = os.path.join(DOWNLOADS_DIR, name)
+        #print(link)
+        if not os.path.exists(DOWNLOADS_DIR):
+            os.makedirs(DOWNLOADS_DIR)
+        if not os.path.isfile(filename):
+            print('Downloading: ' + filename)
+            try:
+                urllib.request.urlretrieve(link, filename)
+            except Exception as inst:
+                print(inst)
+                print('  Encountered unknown error. Continuing.')
+
+if __name__ == "__main__":
+    main(sys.argv[1],sys.argv[2],sys.argv[3])
+    
